415093a8d52f3ab088c7ca171f8a1a1c4285b264
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-05-21 22:33:40 -0500

    the i am poor challenge

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+// the main function is the start of every app
+void main() {
+  runApp(IAmPoor());
+}
+
+class IAmPoor extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am poor'),
+          backgroundColor: Colors.deepPurple[400],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/charcoal.jpeg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
