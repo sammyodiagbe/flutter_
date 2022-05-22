65f01dfa64b07e776dd5a1f5d7ce0ba2df903ab8
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-05-21 22:39:28 -0500

    adding icon launcher to the app

--- i_am_poor/lib/main.dart
+++ i_am_poor/lib/main.dart
@@ -9,6 +9,7 @@ class IAmPoor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('I am poor'),
